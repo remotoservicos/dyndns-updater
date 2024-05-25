7951269ed1275dea72229a8d488651461cbac0e2
Alex Souza <93635630+remotoservicos@users.noreply.github.com>
2024-05-25 16:29:13 -0300

    Update dyndns-updater.sh

--- dyndns-updater.sh
+++ dyndns-updater.sh
@@ -16,6 +16,6 @@ currentIp=$(curl -4 ipconfig.io)
 resolvedDomain=$(dig +short $domain @resolver1.opendns.com)
 
 # Check if the resolved IP address of the domain matches the current IP address and request update, if necessary
-if [[ "$domain" != "$currentIp" ]]; then
+if [[ "$resolvedDomain" != "$currentIp" ]]; then
     curl "https://"$username":"$updaterClientKey"@members.dyndns.org/v3/update?hostname="$domain"&myip="$currentIp""
 fi
